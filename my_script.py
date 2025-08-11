ccbbd4d31ca6b77b12fe36423cabc6f304847463
NatiliKE <mentornatilike@gmail.com>
2025-08-11 22:38:39 +0300

    Add files via upload

---
+++ my_script.py
@@ -0,0 +1,9 @@
+age = 25          # int (integer)
+height = 5.75      # float (decimal)
+name = "Charlie"   # str (string)
+is_student = True  # bool (boolean)
+
+print("Name:", name)          # Outputs: Name: Charlie
+print("Age:", age)            # Outputs: Age: 25
+print("Height:", height)      # Outputs: Height: 5.75
+print("Is a student?", is_student)  # Outputs: Is a student? True
